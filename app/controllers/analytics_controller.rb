8df61e1230482676cd8b3fd0b97bd6e828c6ac9c
derekprovance <dprovance@rednovalabs.com>
2015-08-15 11:26:25 -0500

    Styling updates

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,2 +1,4 @@
 class AnalyticsController < ApplicationController
+  # TODO - need to create an analytics worker that updates daily the new analytics
+  # TODO - obviously need to build the analytics graphs
 end
--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -29,7 +29,7 @@ class GoalsController < ApplicationController
 
     respond_to do |format|
       if @goal.save
-        format.html { redirect_to @goal, notice: 'Goal was successfully created.' }
+        format.html { redirect_to @goal, notice: 'Goal was successfully updated.'}
         format.json { render :show, status: :created, location: @goal }
       else
         format.html { render :new }
@@ -57,7 +57,7 @@ class GoalsController < ApplicationController
   def destroy
     @goal.destroy
     respond_to do |format|
-      format.html { redirect_to goals_url, notice: 'Goal was successfully destroyed.' }
+      # format.html { redirect_to goals_url, notice: 'Goal was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
