88a857faca3cd18a581292753cdfe0ad63dadc51
derekprovance <dprovance@rednovalabs.com>
2015-08-18 23:39:03 -0500

    Fixed broken edit

--- app/controllers/misc_tasks_controller.rb
+++ app/controllers/misc_tasks_controller.rb
@@ -99,7 +99,7 @@ class MiscTasksController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_misc_task
-    @misc_tasks = MiscTask.find(params[:id]) if params[:id].is_a? Numeric
+    @misc_tasks = MiscTask.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
